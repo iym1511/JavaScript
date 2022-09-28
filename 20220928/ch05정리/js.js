6f5b2a6b3822dcda1eefd7664e65bdae76f8d186
iy moon <iym1511@naver.com>
2022-09-28 17:34:37 +0900

    20220928

---
+++ 20220928/ch05정리/js.js
@@ -0,0 +1,6 @@
+// 바닐라 자바스크립트 : 순수 자바스크립트로만 작업
+// jquery 자바스크립트 라이브러리와 함께 사용하는 경우가 많다.
+const title = document.querySelector("#title");
+title.className = "on"; // css에서 가져온것 // 클래스를 통채로 바꿈
+ // classList를 사용하면 add, remove를 통해서 원하는 클래스 이름만 변경
+title.classList.add("off");
