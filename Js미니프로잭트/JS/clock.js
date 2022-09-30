17b731a74018314efe03342d26c865eb94dea858
iy moon <iym1511@naver.com>
2022-09-30 17:23:37 +0900

    20220930

--- Js미니프로잭트/JS/clock.js
+++ Js미니프로잭트/JS/clock.js
@@ -3,7 +3,7 @@ const clock = document.querySelector("#clock");
 
 // 인터벌 함수에 콜백함수로 넣어주기 위함
 function getClock(){
-    // 현재 시간을 가져오는 객체
+    // 현재 시간을 가져오는 객체 (함수안에 객체 넣음)
     const date = new Date();
     // 00:00:00 포맷을 맞추기 위해 String의 padStart메소드 사용
     // 숫자형을 문자형으로 바꿀때 앞으로 숫자로 연산할 일이 있는 지 생각하고 바꾸는게 좋다.
